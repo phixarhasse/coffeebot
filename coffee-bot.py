e830f601a4b0e4dc6e9ade752b2bb492038384d6
Joel Andersson <joel.andersson@spinit.se>
2021-02-04 12:45:59 +0100

    Add emotes and bugfix

--- coffee-bot.py
+++ coffee-bot.py
@@ -6,13 +6,13 @@ import time
 SENSOR_URL = "url-to-shelly-plug/status"
 SLACK_URL = ""
 HEADERS = {"Content-Type":"application/json"}
-SLACK_MESSAGES = {"1bryggs":"Nu bryggs det 1 kanna!",
-                  "2bryggs":"Nu bryggs det 2 kannor!",
-                  "1klar":"1 kanna färdig!",
-                  "2klar":"2 kannor färdiga!",
+SLACK_MESSAGES = {"1bryggs":"Nu bryggs det 1 kanna! :building_construction:",
+                  "2bryggs":"Nu bryggs det 2 kannor! :building_construction: :building_construction:",
+                  "1klar":"1 kanna färdig! :coffee:",
+                  "2klar":"2 kannor färdiga! :coffee: :coffee:",
                   "1gammal":"Någon värmer 1 kanna gammalt kaffe. Mums!",
                   "2gammla":"Någon värmer 2 kannor gammalt kaffe. Mums!",
-                  "slut":"Bryggare avstängd."}
+                  "slut":"Bryggare avstängd. :broken_heart:"}
 
 # Dict to represent brewer state
 STATE = {"brewing": False, "sentEndMessage": True, "onePotDone": False, "twoPotsDone": False}
@@ -25,16 +25,15 @@ def measure():
         print(e)
         return -1.0
     value1 = float(response.json()['meters'][0]['power'])
-    print(value1 + " Watt")
+    print(value1,"Watt")
     time.sleep(MEASURE_INTERVAL)
-
     try:
         response = requests.request("GET", SENSOR_URL)
     except Exception as e:
         print(e)
         return -1.0
     value2 = float(response.json()['meters'][0]['power'])
-    print(value2 + " Watt")
+    print(value2, "Watt")
     # If diff is larger than 10, the power is still changing
     if(not abs(value1 - value2) > 10): return value2
     else: return -1.0
@@ -60,6 +59,7 @@ while(True):
         slackresponse = requests.post(SLACK_URL, headers = HEADERS, json = {"text": SLACK_MESSAGES["1gammal"]})
         print("Slack: " + slackresponse.text)
         STATE["onePotDone"] = True
+        STATE["sentEndMessage"] = False
 
     # Heating 1 fresh pot
     elif((power > 0.0) and (power <= 100.0) and STATE["brewing"]):
@@ -75,6 +75,7 @@ while(True):
         slackresponse = requests.post(SLACK_URL, headers = HEADERS, json = {"text": SLACK_MESSAGES["2gammla"]})
         print("Slack: " + slackresponse.text)
         STATE["twoPotsDone"] = True
+        STATE["sentEndMessage"] = False
         
     # Heating 2 fresh pots
     elif((power > 100.0) and (power <= 300.0) and STATE["brewing"]):
