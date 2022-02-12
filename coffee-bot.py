656e50cae52ef8a21a256b694655b8e1eec0141a
Joel Andersson <joeland@student.chalmers.se>
2022-02-12 21:00:14 +0100

    Major refactoring
    Break out messaging and Hue controls to methods, simplifying main loop layout. Remove pot logging. Bump version number.

--- coffee-bot.py
+++ coffee-bot.py
@@ -1,13 +1,17 @@
-# Version 2.0
+# Version 2.0.1
 # Shelly API doc: https://shelly-api-docs.shelly.cloud/
 # This code is calibrated for a Moccamaster KBG744 AO-B (double brewer with 2 pots).
+
+'''
+TODO: Set up environment variables.
+Add (better) documentation.
+'''
+
 import requests
 import time
-from datetime import date
-from datetime import datetime
 from hue import Hue
 
-SENSOR_URL = "http://url-to-shelly/meter/0"
+SENSOR_URL = "http://ip-to-shelly/meter/0"
 HUE_IP = "ip-to-hue-bridge"
 SLACK_URL = ""
 
@@ -18,12 +22,15 @@ MESSAGES = {"1bryggs":"Nu bryggs det 1 kanna! :building_construction:",
                   "slut":"Bryggare avstängd. :broken_heart:",
                   "svalnande": "Någon räddar svalnande kaffe! :ambulance:"}
 
-# Dict to represent brewer state
+# Dict representing brewer state
 STATE = {"brewing": False, "sentEndMessage": True, "coffeeDone": False}
 MEASURE_INTERVAL = 5 #seconds
-SEND_TO_SLACK = False
+SEND_TO_SLACK = True
+USE_HUE = True
 
-"""Polls the Shelly embedded web server for power usage [Watt] twice with MEASURE_INTERVAL seconds between.
+
+"""
+Polls the Shelly embedded web server for power usage [Watt] twice with MEASURE_INTERVAL seconds between.
 Returns second value if valid measure, -1.0 otherwise.
 """
 def measure():
@@ -38,6 +45,7 @@ def measure():
     if(value1 > 2000.0):
         tolerance = 80
     print(value1,"Watt")
+
     time.sleep(MEASURE_INTERVAL)
     try:
         response = requests.request("GET", SENSOR_URL)
@@ -46,6 +54,7 @@ def measure():
         return -1.0
     value2 = float(response.json()['power'])
     print(value2, "Watt")
+
     # If diff is larger than 10, the power is still changing
     # Diffs lower than 1.0 should not trigger anything
     if(value1 == 0.0 and value2 == 0.0): return value2
@@ -53,43 +62,33 @@ def measure():
         abs(value1 - value2) > 1.0): return value2
     else: return -1.0
 
+"""Resets the global dict respresenting the brewer state
+"""
 def resetState():
     STATE["brewing"] = False
     STATE["sentEndMessage"] = True
     STATE["coffeeDone"] = False
 
-def writeStatsToFile(pots):
-    today = date.today()
-    now = datetime.now()
-    if(not type(pots) is int):
-        print("Input to file not an integer")
-        return -1
-    try:
-        # Open log file, creates one if does not exist
-        f = open(f"./logs/stats-{today}.stat", "a")
-        f.write(f"{now}|{pots}\n")
-        f.close()
-        return 0
-    except Exception as e:
-        print(e)
-        return -1
-
-def main():
+def setupHue():
     hue = Hue(HUE_IP)
-
     # Get Hue username
     hue.loadUsername()
     if(hue.username == ""):
         print("Waiting for Hue authorization...")
         hue.authorize()
         print("---> Hue Authorization complete!")
-
     # Get Hue lights
     hue.getLights()
-
     # Set all lights to red
     hue.setAllLights(65000)
+    return hue
 
+'''
+Main loop
+'''
+def main():
+    if(USE_HUE):
+        hue = setupHue()
     # Main loop
     while(True):
         power = measure()
@@ -100,71 +99,103 @@ def main():
 
         # Heating old coffee
         elif((power > 1.0) and (power <= 300.0) and not STATE["brewing"] and not STATE["coffeeDone"]):
-            print(MESSAGES["svalnande"])
-            if(SEND_TO_SLACK):
-                slackresponse = requests.post(SLACK_URL, headers = HEADERS, json = {"text": MESSAGES["svalnande"]})
-                print(f"Slack: {slackresponse.text}")
-            hue.setAllLights(29000) # green
-            STATE["coffeeDone"] = True
-            STATE["sentEndMessage"] = False
+            heatingOldCoffee(hue)
 
         # Fresh coffee has been made
         elif((power > 1.0) and (power <= 300.0) and STATE["brewing"]):
-            time.sleep(30) # Wait for coffee to drip down
-            print(MESSAGES["klart"])
-            if(SEND_TO_SLACK):
-                slackresponse = requests.post(SLACK_URL, headers = HEADERS, json = {"text": MESSAGES["klart"]})
-                print(f"Slack: {slackresponse.text}")
-            hue.setAllLights(29000) # green
-            STATE["coffeeDone"] = True
-            STATE["brewing"] = False
+            freshCoffeeHasBeenMade(hue)
 
         # One pot is brewing
         elif(power > 1000.0 and power <= 2000.0 and not STATE["brewing"]):
-            print(MESSAGES["1bryggs"])
-            if(SEND_TO_SLACK):
-                slackresponse = requests.post(SLACK_URL, headers = HEADERS, json = {"text": MESSAGES["1bryggs"]})
-                print(f"Slack: {slackresponse.text}")
-            hue.setAllLights(10000) # yellow
-            STATE["brewing"] = True
-            STATE["sentEndMessage"] = False
-            filewritten = writeStatsToFile(1)
-            if(filewritten == -1):
-                print("Failed to write stats to file.")
+            onePotIsBrewing(hue)
         
         # Still brewing, make lights blink
         elif(power > 1000.0 and STATE["brewing"]):
-            hue.turnOffAllLights()
-            time.sleep(1)
-            hue.setAllLights(10000) # yellow
+            stillBrewing(hue)
 
         # Two pots are brewing
         elif(power > 2000.0 and not STATE["brewing"]):
-            print(MESSAGES["2bryggs"])
-            if(SEND_TO_SLACK):
-                slackresponse = requests.post(SLACK_URL, headers = HEADERS, json = {"text": MESSAGES["2bryggs"]})
-                print(f"Slack: {slackresponse.text}")
-            hue.setAllLights(10000) # yellow
-            STATE["brewing"] = True
-            STATE["sentEndMessage"] = False
-            filewritten = writeStatsToFile(2)
-            if(filewritten == -1):
-                print("Failed to write stats to file.")
+            twoPotsAreBrewing(hue)
 
         # Coffee maker turned off
         elif(power == 0.0 and not STATE["sentEndMessage"]):
-            print(MESSAGES["slut"])
-            if(SEND_TO_SLACK):
-                slackresponse = requests.post(SLACK_URL, headers = HEADERS, json = {"text": MESSAGES["slut"]})
-                print(f"Slack: {slackresponse.text}")
-            hue.setAllLights(65000) # red
-            resetState()
+            coffeeMakerTurnedOff(hue)
 
         # Idle, don't send messages
         elif(power == 0.0 and STATE["sentEndMessage"]):
             print("Bryggare avstängd.")
 
         time.sleep(MEASURE_INTERVAL)
+
+'''
+Messaging and Hue control methods
+'''
+def heatingOldCoffee(hue):
+    print(MESSAGES["svalnande"])
+    if(SEND_TO_SLACK):
+        slackresponse = requests.post(SLACK_URL, headers = HEADERS, json = {"text": MESSAGES["svalnande"]})
+        print(f"Slack: {slackresponse.text}")
+
+    if(USE_HUE):
+        hue.setAllLights(29000) # green
+
+    STATE["coffeeDone"] = True
+    STATE["sentEndMessage"] = False
+
+def freshCoffeeHasBeenMade(hue):
+    time.sleep(30) # Wait for coffee to drip down
+    print(MESSAGES["klart"])
+    if(SEND_TO_SLACK):
+        slackresponse = requests.post(SLACK_URL, headers = HEADERS, json = {"text": MESSAGES["klart"]})
+        print(f"Slack: {slackresponse.text}")
+
+    if(USE_HUE):
+        hue.setAllLights(29000) # green
+
+    STATE["coffeeDone"] = True
+    STATE["brewing"] = False
+
+def onePotIsBrewing(hue):
+    print(MESSAGES["1bryggs"])
+    if(SEND_TO_SLACK):
+        slackresponse = requests.post(SLACK_URL, headers = HEADERS, json = {"text": MESSAGES["1bryggs"]})
+        print(f"Slack: {slackresponse.text}")
+
+    if(USE_HUE):
+        hue.setAllLights(10000) # yellow
+
+    STATE["brewing"] = True
+    STATE["sentEndMessage"] = False
+
+def twoPotsAreBrewing(hue):
+    print(MESSAGES["2bryggs"])
+    if(SEND_TO_SLACK):
+        slackresponse = requests.post(SLACK_URL, headers = HEADERS, json = {"text": MESSAGES["2bryggs"]})
+        print(f"Slack: {slackresponse.text}")
+
+    if(USE_HUE):
+        hue.setAllLights(10000) # yellow
+
+    STATE["brewing"] = True
+    STATE["sentEndMessage"] = False
+
+def stillBrewing(hue):
+    if(USE_HUE):
+        hue.turnOffAllLights()
+        time.sleep(1)
+        hue.setAllLights(10000) # yellow
+
+def coffeeMakerTurnedOff(hue):
+    print(MESSAGES["slut"])
+    if(SEND_TO_SLACK):
+        slackresponse = requests.post(SLACK_URL, headers = HEADERS, json = {"text": MESSAGES["slut"]})
+        print(f"Slack: {slackresponse.text}")
+
+    if(USE_HUE):
+        hue.setAllLights(65000) # red
+
+    resetState()
+
 
 if(__name__ == "__main__"):
     main()
