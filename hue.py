4e14cd527f21c46e4967eb2ef480cf215aa6873c
phixarhasse <joeland@student.chalmers.se>
2024-03-14 10:35:38 +0100

    Logging, stability and MongoDb integration (#26)
    * Add MongoDb class and env vars
    
    * Use formatted logging
    
    * Formatting
    
    * Integrate MongoDb data storage
    Add formatted logging, make app async and allow integrations to be optional
    
    ---------
    
    Co-authored-by: Joel Andersson <joel.andersson@spinit.se>

--- hue.py
+++ hue.py
@@ -1,25 +1,26 @@
 import os
+import logging
 import requests
-from dotenv import load_dotenv
+
 
 class Hue:
     def __init__(self):
-        load_dotenv('.env')
+        logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s',
+                            level=logging.DEBUG, datefmt="%Y-%m-%d %H:%M:%S")
         try:
-            self.useHue = os.environ['USE_HUE']
-            self.bridgeIp = os.environ['HUE_IP']
+            self.bridgeIp = os.getenv('HUE_IP')
             self.url = f"http://{self.bridgeIp}/api"
         except KeyError:
-            print("Could not parse USE_HUE or HUE_IP in the file .env")
-            quit()
-            
+            logging.error("Could not parse HUE_IP in the file .env")
+            quit(1)
+
         self.lights = []
         self.username = ""
         self.loadUsername()
-        if(self.username == ""):
-            print("Waiting for Hue authorization...")
+        if (self.username == ""):
+            logging.info("Waiting for Hue authorization...")
             self.authorize()
-            print("---> Hue Authorization complete!")
+            logging.info("---> Hue Authorization complete!")
 
     def saveUsername(self, username):
         try:
@@ -27,76 +28,80 @@ class Hue:
             f.write(username)
             f.close()
         except Exception as e:
-            print(e)
-    
+            logging.error(e)
+
     def loadUsername(self):
         try:
             f = open("hue_username", "r")
             self.username = f.readline()
             f.close()
         except Exception as e:
-            print(e)
+            logging.error(e)
             self.username = ""
             return
 
     def authorize(self):
         self.username = ""
         try:
-            hueResponse = requests.post(self.url, json={"devicetype": "coffeebot"})
-            if(hueResponse.json()[0]["error"]["type"] == 101): # Need to generate username
-                print("Please press the link button on the HUE Bridge.")
+            hueResponse = requests.post(
+                self.url, json={"devicetype": "coffeebot"})
+            # Need to generate username
+            if (hueResponse.json()[0]["error"]["type"] == 101):
+                logging.info(
+                    "\tPlease press the link button on the HUE Bridge.")
                 user_input = input("Have you pressed it? [y/n] ")
-                if(not user_input == 'y'):
-                    print("Error during Hue authentication. Exiting.")
-                    exit(1)
+                if (not user_input == 'y'):
+                    logging.info("\tHue authentication cancelled. Exiting.")
+                    quit(0)
                 else:
-                    hueResponse = requests.post(self.url, json={"devicetype": "coffeebot"})
+                    hueResponse = requests.post(
+                        self.url, json={"devicetype": "coffeebot"})
                     username = hueResponse.json()[0]["success"]["username"]
                     self.username = username
                     self.saveUsername(username)
-            elif(hueResponse.ok):
+            elif (hueResponse.ok):
                 username = hueResponse.json()[0]["success"]["username"]
                 self.username = username
                 self.saveUsername(username)
         except Exception as e:
-            print("Error during Hue authentication.")
-            print("Exception: ", e)
-            return
+            logging.error("Error during Hue authentication.")
+            logging.error("Exception: ", e)
+            quit(1)
 
     def getLights(self):
         self.lights = []
-        if(self.username == ""):
+        if (self.username == ""):
             return
         try:
             hueResponse = requests.get(f"{self.url}/{self.username}/lights/")
-            if(not hueResponse.ok):
-                print("Unable to get Hue lights.")
+            if (not hueResponse.ok):
+                logging.warning("Unable to get Hue lights.")
                 return
         except Exception as e:
-            print(e)
+            logging.error(e)
             return
         for light in hueResponse.json():
             self.lights.append(light)
         return
-    
+
     def setAllLights(self, color):
         try:
             for light in self.lights:
                 hueResponse = requests.put(
                     f"{self.url}/{self.username}/lights/{light}/state",
-                    json={"on":True, "sat": 254, "bri":200, "hue": color})
-                print(f"Hue light {light}: {hueResponse.status_code}")
+                    json={"on": True, "sat": 254, "bri": 200, "hue": color})
+                logging.debug(f"Hue light {light}: {hueResponse.status_code}")
         except Exception as e:
-            print(e)
+            logging.error(e)
             return
-    
+
     def turnOffAllLights(self):
         try:
             for light in self.lights:
                 hueResponse = requests.put(
                     f"{self.url}/{self.username}/lights/{light}/state",
-                    json={"on":False})
-                print(f"Hue light {light}: {hueResponse.status_code}")
+                    json={"on": False})
+                logging.debug(f"Hue light {light}: {hueResponse.status_code}")
         except Exception as e:
-            print(e)
+            logging.error(e)
             return
